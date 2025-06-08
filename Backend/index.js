ddb496ebdb4fdd6994fbf99108b493387f1ca376
Sahil Kavatkar <sahilkavatkar@gmail.com>
2025-06-08 17:10:19 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,11 @@
-console.info("Hello")
+import express from 'express';
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send({message:"hello sahil"});
+})
+
+app.listen( 8080, ()=>{
+    console.log("server is running on port 8080")
+})
